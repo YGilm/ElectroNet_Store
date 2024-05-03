a42f77c96a95367f2f9ed31bc98c8db84fa38365
yury gilm <ygilm@bk.ru>
2024-05-03 23:46:05 +0300

    Added views for models application

--- network/views.py
+++ network/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-# Create your views here.
+from network.filters import CountryFilter
+from network.models import BusinessUnit, Product
+from network.permissions import IsActiveStaff
+from network.serializers import BusinessUnitSerializer, ProductSerializer
+
+
+class BusinessUnitViewSet(viewsets.ModelViewSet):
+    queryset = BusinessUnit.objects.all()
+    serializer_class = BusinessUnitSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = CountryFilter
+    permission_classes = [IsActiveStaff]
+
+
+class ProductListCreateAPIView(ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsActiveStaff]
+
+
+class ProductRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsActiveStaff]
