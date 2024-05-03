4c45e9155a10210fda42289de666c5632f46d15e
yury gilm <ygilm@bk.ru>
2024-05-03 23:44:51 +0300

    Added endpoints products

--- network/urls.py
+++ network/urls.py
@@ -1,5 +1,17 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BusinessUnitViewSet, ProductListCreateAPIView, ProductRetrieveUpdateDestroyAPIView
+
 app_name = 'network'
 
-urlpatterns = [
+router = DefaultRouter()
+router.register(r'businessunits', BusinessUnitViewSet)
 
+urlpatterns = [
+    # BusinessUnit
+    path('', include(router.urls)),
+
+    # Product
+    path('products/', ProductListCreateAPIView.as_view(), name='product-list-create'),
+    path('products/<pk>/', ProductRetrieveUpdateDestroyAPIView.as_view(), name='product-detail'),
 ]
